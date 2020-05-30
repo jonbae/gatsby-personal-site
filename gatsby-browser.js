85b93a6cf1ecef130bd4d0d2a53c00865d4e7a38
jonbae <jonbae61596@gmail.com>
2020-05-30 19:29:41 -0400

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { ThemeContextProvider } from "./src/theme-context"
-require("prismjs/themes/prism.css")
-
-export const wrapRootElement = ({ element }) => (
-  <ThemeContextProvider>{element}</ThemeContextProvider>
-);
